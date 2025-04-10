5e51ab1c3a9b8f3cad7d95fa22c11e0532f8dbb7
Amri Karisma <amrikarisma@live.com>
2025-04-10 16:38:59 +0700

    fix convert data

--- src/ESP32_CANBUS.ino
+++ src/ESP32_CANBUS.ino
@@ -38,7 +38,8 @@
 //     vTaskDelay(send_speed_interval / portTICK_PERIOD_MS);
 //   }
 // }
-static unsigned long lastPrintTime = 0; // Variabel untuk menyimpan waktu terakhir print
+static unsigned long lastPrintTime = 0; 
+static unsigned long lastFristRunTime = 0; 
 uint16_t global_rpm = 0;
 uint16_t global_map = 0;
 uint16_t global_tps = 0;
@@ -47,7 +48,8 @@ float global_afr = 0.0;
 uint16_t global_vss = 0;
 float global_voltage = 0.0;
 uint16_t global_clt = 0;
-
+uint16_t global_iat = 0;
+boolean first_run = true;
 void setup()
 {
 	Serial.begin(115200);
@@ -64,7 +66,7 @@ void setup()
 
 	// Serial.println("ESP32-NOW initialized");
 	CAN0.setCANPins(GPIO_NUM_16, GPIO_NUM_17); // see important note above
-	CAN0.begin(500000);						   // 500Kbps
+	// CAN0.begin(500000);						   // 500Kbps
 	if (CAN0.begin(500000))
 	{
 		Serial.println("CAN driver initialized successfully.");
@@ -73,6 +75,8 @@ void setup()
 	{
 		Serial.println("Failed to initialize CAN driver.");
 	}
+	delay(5000);
+
 	// Filter untuk ID tertentu berdasarkan dokumen Haltech
 	CAN0.watchFor(0x360); // RPM, MAP, TPS
 	CAN0.watchFor(0x361); // Fuel Pressure
@@ -80,84 +84,103 @@ void setup()
 	CAN0.watchFor(0x370); // VSS
 	CAN0.watchFor(0x372); // Voltage
 	CAN0.watchFor(0x3E0); // CLT
-	delay(500);
 }
 
 void loop()
 {
-    unsigned long currentTime = millis(); // Waktu saat ini
+	unsigned long currentTime = millis(); // Waktu saat ini
 
-    if (CAN0.available())
-    { // Periksa apakah ada pesan di buffer
-        CAN_FRAME can_message;
-        if (CAN0.read(can_message))
-        {
-            // Proses data berdasarkan ID
-            switch (can_message.id)
-            {
-            case 0x360:
-            {																			 // RPM, MAP, TPS
-                global_rpm = (can_message.data.byte[0] << 8) | can_message.data.byte[1]; // Byte 0-1
-                global_map = (can_message.data.byte[2] << 8) | can_message.data.byte[3]; // Byte 2-3
-                global_tps = (can_message.data.byte[4] << 8) | can_message.data.byte[5]; // Byte 4-5
-                break;
-            }
-            case 0x361:
-            {																					   // Fuel Pressure
-                global_fuel_pressure = (can_message.data.byte[0] << 8) | can_message.data.byte[1]; // Byte 0-1
-                break;
-            }
-            case 0x368:
-            {																			   // AFR 01
-                uint16_t afr = (can_message.data.byte[0] << 8) | can_message.data.byte[1]; // Byte 0-1
-                global_afr = afr / 100.0;												   // Konversi ke nilai desimal
-                break;
-            }
-            case 0x370:
-            {																			 // VSS
-                global_vss = (can_message.data.byte[0] << 8) | can_message.data.byte[1]; // Byte 0-1
-                break;
-            }
-            case 0x372:
-            {																				   // Voltage
-                uint16_t voltage = (can_message.data.byte[0] << 8) | can_message.data.byte[1]; // Byte 0-1
-                global_voltage = voltage / 1000.0;											   // Konversi ke volt
-                break;
-            }
-            case 0x3E0:
-            {																			 // CLT
-                global_clt = (can_message.data.byte[0] << 8) | can_message.data.byte[1]; // Byte 0-1
-                break;
-            }
-            default:
-                break;
-            }
-        }
-    }
+	if (CAN0.available())
+	{ // Periksa apakah ada pesan di buffer
+		if (!first_run)
+		{
+			CAN_FRAME can_message;
+			if (CAN0.read(can_message))
+			{
+				// Proses data berdasarkan ID
+				switch (can_message.id)
+				{
+				case 0x360:
+				{																			   // RPM, MAP, TPS
+					global_rpm = (can_message.data.byte[0] << 8) | can_message.data.byte[1];   // Byte 0-1
+					uint16_t map = (can_message.data.byte[2] << 8) | can_message.data.byte[3]; // Byte 2-3
+					uint16_t tps = (can_message.data.byte[4] << 8) | can_message.data.byte[5]; // Byte 4-5
+					global_map = map / 10.0;												   // Konversi ke kPa
+					global_tps = tps / 10.0;												   // Konversi ke kPa
+					break;
+				}
+				case 0x361:
+				{																						 // Fuel Pressure
+					uint16_t fuel_pressure = (can_message.data.byte[0] << 8) | can_message.data.byte[1]; // Byte 0-1
+					global_fuel_pressure = fuel_pressure / 10 - 101.3;
+					break;
+				}
+				case 0x368:
+				{																				   // AFR 01
+					uint16_t afr_raw = (can_message.data.byte[0] << 8) | can_message.data.byte[1]; // Byte 0-1
+					float lambda = afr_raw / 1000.0;
+					global_afr = lambda * 14.7;
+					break;
+				}
+				case 0x370:
+				{																			   // VSS
+					uint16_t vss = (can_message.data.byte[0] << 8) | can_message.data.byte[1]; // Byte 0-1
+					global_vss = vss / 10.0;												   // Konversi ke km/h
+					break;
+				}
+				case 0x372:
+				{																				   // Voltage
+					uint16_t voltage = (can_message.data.byte[0] << 8) | can_message.data.byte[1]; // Byte 0-1
+					global_voltage = voltage / 10.0;											   // Konversi ke volt
+					break;
+				}
+				case 0x3E0:
+				{																				   // CLT
+					uint16_t clt_raw = (can_message.data.byte[0] << 8) | can_message.data.byte[1]; // Byte 0-1
+					uint16_t iat_raw = (can_message.data.byte[2] << 8) | can_message.data.byte[3]; // Byte 0-1
+					float clt_k = clt_raw / 10.0;
+					float iat_k = iat_raw / 10.0;
 
-    // Cek apakah sudah waktunya untuk print
-    if (currentTime - lastPrintTime >= 1000)
-    { // Interval 1000ms
-        Serial.print("RPM: ");
-        Serial.print(global_rpm);
-        Serial.print(" MAP: ");
-        Serial.print(global_map);
-        Serial.print(" kPa TPS: ");
-        Serial.print(global_tps);
-        Serial.print(" % Fuel Pressure: ");
-        Serial.print(global_fuel_pressure);
-        Serial.print(" kPa AFR: ");
-        Serial.print(global_afr, 2);
-        Serial.print(" VSS: ");
-        Serial.print(global_vss);
-        Serial.print(" km/h Voltage: ");
-        Serial.print(global_voltage, 2);
-        Serial.print(" V CLT: ");
-        Serial.print(global_clt);
-        Serial.println(" °C");
+					global_clt = clt_k - 273.15;
+					global_iat = iat_k - 273.15;
+					break;
+				}
+				default:
+					break;
+				}
+			}
+		}
+		else
+		{
+			if (currentTime - lastFristRunTime >= 5000)
 
-        lastPrintTime = currentTime; // Update waktu terakhir print
-    }
-    yield();   // Beri kesempatan untuk tugas lain
-    delay(20); // 50Hz = 1000ms / 50 = 20ms
+				first_run = false;
+		}
+	}
+	if (currentTime - lastPrintTime >= 1000)
+	{ // Interval 1000ms
+		Serial.print("RPM: ");
+		Serial.print(global_rpm);
+		Serial.print(" MAP: ");
+		Serial.print(global_map);
+		Serial.print(" kPa TPS: ");
+		Serial.print(global_tps);
+		Serial.print(" % Fuel Pressure: ");
+		Serial.print(global_fuel_pressure);
+		Serial.print(" kPa AFR: ");
+		Serial.print(global_afr, 2);
+		Serial.print(" VSS: ");
+		Serial.print(global_vss);
+		Serial.print(" km/h Voltage: ");
+		Serial.print(global_voltage, 2);
+		Serial.print(" V CLT: ");
+		Serial.print(global_clt);
+		Serial.print(" °C IAT: ");
+		Serial.print(global_iat);
+		Serial.println(" °C");
+
+		lastPrintTime = currentTime; // Update waktu terakhir print
+	}
+	yield();   // Beri kesempatan untuk tugas lain
+	delay(20); // 50Hz = 1000ms / 50 = 20ms
 }
